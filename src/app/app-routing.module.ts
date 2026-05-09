d0dae43a3139d4faa36adf3e668b09c01bbe13af
El-boujamai <ouardiaelboujamaai70@gmail.com>
2026-05-09 16:26:25 +0100

    la partie 4' ajoute  de service auth etcompenet login'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { Login } from './login/login';
 import { RouterModule, Routes } from '@angular/router';
 import {CustomersComponent} from "./customers/customers.component";
 import {AccountsComponent} from "./accounts/accounts.component";
@@ -6,6 +7,8 @@ import {NewCustomerComponent} from "./new-customer/new-customer.component";
 import {CustomerAccountsComponent} from "./customer-accounts/customer-accounts.component";
 
 const routes: Routes = [
+  { path: 'login', component: Login },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path :"customers", component : CustomersComponent},
   { path :"accounts", component : AccountsComponent},
   { path :"new-customer", component : NewCustomerComponent},
